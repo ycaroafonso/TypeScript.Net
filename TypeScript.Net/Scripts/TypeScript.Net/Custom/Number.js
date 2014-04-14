b15f8e4fc3651e796208dfec3d2ead2cc8217540
Ycaro Afonso <ycaroafonso@gmail.com>
2014-04-14 18:22:29 -0400

    ToMoney

--- TypeScript.Net/Scripts/TypeScript.Net/Custom/Number.js
+++ TypeScript.Net/Scripts/TypeScript.Net/Custom/Number.js
@@ -4,4 +4,11 @@ Number.prototype.ToRound = function (digits) {
     var pow10 = Math.pow(10, digits);
     return Math.round(this * pow10) / pow10;
 };
+
+Number.prototype.ToMoney = function () {
+    var val = this == "" || this == null ? parseFloat(0) : this;
+    val = val.toFixed(2);
+    val = val.replace(".", ",");
+    return val;
+};
 //# sourceMappingURL=Number.js.map
--- TypeScript.Net/Scripts/TypeScript.Net/Custom/Number.ts
+++ TypeScript.Net/Scripts/TypeScript.Net/Custom/Number.ts
@@ -1,5 +1,6 @@
 interface Number {
     ToRound(digits: number): number;
+    ToMoney(): string;
 }
 Number.prototype.ToRound = function (digits: number) {
     if (digits < 0 || digits > 10)
@@ -7,3 +8,10 @@ Number.prototype.ToRound = function (digits: number) {
     var pow10 = Math.pow(10, digits);
     return Math.round(this * pow10) / pow10;
 }
+
+Number.prototype.ToMoney = function () {
+    var val = this == "" || this == null ? parseFloat(0) : this;
+    val = val.toFixed(2);
+    val = val.replace(".", ",");
+    return val;
+};
